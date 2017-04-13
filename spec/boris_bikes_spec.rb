4a82a94b5a3ebec81348a704852c83256554ec87
Anthony Crisp <anthony_crisp@hotmail.com>
2017-04-13 14:35:07 +0100

    Challemge 11

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,10 +1,22 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
 
   it 'releases a working bike' do
     bike = subject.release_bike
-    expect(bike).to be_working 
-  end
+    expect(bike).to be_working
+    end
+
+it { is_expected.to respond_to(:dock).with(1).argument}
+
+it { is_expected.to respond_to :bike }
+
+it 'Docks a bike' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
+end
+
 end
