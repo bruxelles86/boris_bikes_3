8223e9e4a9ab1f255bd39d5b7e41ac0b7d15462b
Anthony Crisp <anthony_crisp@hotmail.com>
2017-04-13 18:18:25 +0100

    Challenge 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,13 +4,12 @@ class DockingStation
 
 attr_reader :bike
 
-  def release_bike
-      Bike.new
-  end
-
   def dock(bike)
     @bike = bike
   end
 
-
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,10 +3,23 @@ require 'bike'
 
 describe DockingStation do
 
+  describe'#release_bike' do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+  end
+end
+  describe'#release_bike' do
+    it 'Has no bikes' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+end
+
   it { is_expected.to respond_to :release_bike }
 
   it 'releases a working bike' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
     end
 
@@ -18,5 +31,6 @@ it 'Docks a bike' do
   bike = Bike.new
   expect(subject.dock(bike)).to eq bike
 end
+
 
 end
